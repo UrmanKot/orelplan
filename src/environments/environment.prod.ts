f3916ee77103b1acd5fc72b214ade7837e597f4c
v.trubicin <v.trubicin@unitcode.ru>
2021-09-17 04:26:43 +0300

    Тест

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
-export const environment = {
-  production: true
+import {Environment} from "./interface";
+
+export const environment: Environment = {
+  production: false,
+  apiKey: 'AIzaSyA9biG_XjgBErs-TLHf_njDSrGXoC7dnAA',
+  fbDbUrl: 'https://orelarhplan-default-rtdb.firebaseio.com'
 };
